9be7a5655333a1161e3ae29909c53841827f7074
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2021-12-15 16:12:03 -0500

    .

--- COCI '16 C7 #1 Baza/COCI '16 C7 #1 Baza/main.cpp
+++ COCI '16 C7 #1 Baza/COCI '16 C7 #1 Baza/main.cpp
@@ -7,8 +7,36 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int N, M, Q;
+    int db[1000][1000];
+    int query[50][1000];
+    cin>>N>>M;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            cin>>db[i][j];
+        }
+    }
+    cin>>Q;
+    for(int i=0;i<Q;i++){
+        for(int j=0;j<M;j++){
+            cin>>query[i][j];
+        }
+    }
+    for(int i=0;i<Q;i++){
+        int cnt=N;
+        for(int j=0;j<N;j++){
+            for(int k=0;k<M;k++){
+                if(query[i][k]==-1);
+                else if(query[i][k]!=db[j][k]){
+                    cnt--;
+                    break;
+                }
+            }
+        }
+        printf("%d\n",cnt);
+    }
     return 0;
 }
