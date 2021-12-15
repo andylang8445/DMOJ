00531753e461ce5db1195f8be26bfab333baaa95
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2021-12-15 15:48:51 -0500

    .

---
+++ COCI '16 C7 #1 Baza/COCI '16 C7 #1 Baza/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COCI '16 C7 #1 Baza
+//
+//  Created by Hongjun Yun on 2021-12-15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
