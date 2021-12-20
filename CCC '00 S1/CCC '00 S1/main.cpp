fc8be02528f8ac250da341f68bb5ad2c3be9482d
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2021-12-20 16:41:33 -0500

    .

---
+++ CCC '00 S1/CCC '00 S1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CCC '00 S1
+//
+//  Created by Hongjun Yun on 2021-12-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
