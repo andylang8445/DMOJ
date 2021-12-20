bc8156fb7f85e4d202f0285a9234be0674ebb857
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2021-12-20 16:55:28 -0500

    .

--- CCC '00 S1/CCC '00 S1/main.cpp
+++ CCC '00 S1/CCC '00 S1/main.cpp
@@ -7,8 +7,41 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int coin{0}, m1{0}, m2{0}, m3{0};
+    int cnt{0};
+    cin>>coin>>m1>>m2>>m3;
+    while(coin>0){
+        if(coin>0){
+            coin--;
+            cnt++;
+            m1++;
+        }
+        if(coin>0){
+            coin--;
+            cnt++;
+            m2++;
+        }
+        if(coin>0){
+            coin--;
+            cnt++;
+            m3++;
+        }
+        if(m1%35==0){
+            coin+=30;
+            m1=0;
+        }
+        if(m2%100==0){
+            coin+=60;
+            m2=0;
+        }
+        if(m3%10==0){
+            coin+=9;
+            m3=0;
+        }
+    }
+    cout<<"Martha plays "<<cnt<<" times before going broke.";
     return 0;
 }
