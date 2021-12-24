17ed8e75dd64e6ca14d668150ffc2f2d39de4b05
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2021-12-23 22:17:46 -0500

    .

---
+++ CCC '17 S2/CCC '17 S2/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  CCC '17 S2
+//
+//  Created by Hongjun Yun on 2021-12-23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int N{0};
+    cin>>N;
+    int arr[N];
+    for(int i{0};i<N;++i){
+        cin>>arr[i];
+    }
+    
+    return 0;
+}
